eaffe8e10ee9033a5938d3e44c3f676931d4b2e8
jazownik-tivix <konrad.jazownik@tivix.com>
2020-02-10 10:31:54 +0100

    added selector template

---
+++ _templates/cypress/selector/prompt.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+      type: 'input',
+      name: 'path',
+      message: "Where do you want put new selector?"
+    },
+    {
+      type: 'input',
+      name: 'selector_name',
+      message: "What's your selector name?"
+    },
+    {
+        type: 'input',
+        name: 'selector_value',
+        message: "What's your selector value?"
+    }
+  ]
---
+++ _templates/cypress/selector/selector.ejs.t
@@ -0,0 +1,6 @@
+---
+inject: true
+to: <%= cwd %>/<%= path %>/selectors.js
+append: true
+---
+export const <%= selector_name %> = <%= selector_value %>
